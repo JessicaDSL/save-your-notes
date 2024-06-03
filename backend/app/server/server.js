ba84f8fd7d1ec55387007b3725ecbbf1d75d84d2
Jessica Lima <jessicadlima@multiplan.com.br>
2024-06-03 09:35:48 -0300

    fix: finish endpoint register

--- backend/app/server/server.js
+++ backend/app/server/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import db from '../config/db.js'
+import db from '../config/db.js';
+import bcrypt from 'bcrypt';
+
 
 const app = express();
 const PORT = 4200;
@@ -11,13 +13,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-db.all('SELECT * FROM "notas"', (err, rows) => {
-  if (err) {
-    console.error('Erro ao buscar as notas', err.message);
-    return;
-  }
-  console.log('Notas encontradas:', rows) 
-})
+// db.all('SELECT * FROM "notas"', (err, rows) => {
+//   if (err) {
+//     console.error('Erro ao buscar as notas', err.message);
+//     return;
+//   }
+//   console.log('Notas encontradas:', rows) 
+// })
+
+// db.all('SELECT * FROM "users"', (err, rows) => {
+//   if(err) {
+//     console.error('Erro ao encontrar usuarios', err.message);
+//     return;
+//   }
+//   console.log('Usuarios encontrados: ', rows)
+// })
 
 app.post('/notas', (req, res) => {
   const { title, message, user_id } = req.body;
@@ -40,6 +50,30 @@ app.post('/notas', (req, res) => {
     res.status(201).json({ id: this.lastID, title, message, user_id,  data_createdAt: currentData });
   });
 });
+
+app.post('/register', (req, res) => {
+  const {username, email, senha} = req.body;
+
+  if (!username || !email || !senha) {
+    return res.status(400).json({ error: 'Todos os campos são obrigatórios!' });
+  }
+
+  const hashPassword = bcrypt.hashSync(senha, 10);
+
+  const query = `
+    INSERT INTO users (username, email, senha)
+    VALUES (?, ?, ?)
+  `;
+
+  db.run(query, [username, email, hashPassword], function(err) {
+    if(err) {
+      return res.status(500).json({error: err.message})
+    }
+    res.status(201).json({  username, email})
+  })
+ 
+
+})
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
